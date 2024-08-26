0388ba72aac0b3a06cc75fff4e321cc9519b0892
LouisLazz <louisblazzaro@icloud.com>
2024-08-27 09:15:19 +1000

    Create AL Expense Tracker.py

---
+++ AL Expense Tracker.py
@@ -0,0 +1,7 @@
+# Imports
+
+import customtkinter as ctk
+import matplotlib.pyplot as plt
+from customtkinter import CTkButton, CTkEntry, CTkLabel, CTkTextbox, CTkFrame, CTkScrollbar, CTkOptionMenu, CTkComboBox, CTkProgressBar, CTkSlider
+
+# Functions
